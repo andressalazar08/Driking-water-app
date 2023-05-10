8f1963ceb0cb36ff930f675f7e22ea91b2b1c324
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-10 18:41:59 -0500

    export an import methods

--- Registro.java
+++ Registro.java
@@ -57,7 +57,56 @@ public class Registro {
         this.userId = userId;
     }
 
-    public void exportar(){
-        //TODO: Implementar método para exportar datos del consumo histórico
+    public void exportarArraylist(ArrayList<String> listaDatos, String rutaArchivo)
+    {
+        try
+        {
+            // Crear nuevo archivo para guardar Arraylist
+            FileWriter archivo = new FileWriter(rutaArchivo);
+            // Crea abjeto de la clase  BufferedWriter para preparar escritura en el archivo
+            BufferedWriter bufferEscritura = new BufferedWriter(archivo);
+            // Recorre Array de datos a escribir para llevarlo al nuevo archivo
+            for (String dato : listaDatos)
+            {
+                bufferEscritura.write(dato);
+                bufferEscritura.newLine();
+            }
+            // Cerrar Buffer
+            bufferEscritura.close();
+            archivo.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> importarArraylist(String rutaArchivo)
+    {
+        try
+        {
+            // Objeto clase Filereader clae que se utiliza para recibir caracteres
+            FileReader archivo = new FileReader(rutaArchivo);
+            // Objeto de la clase BufferedReader usado para leer texto de un flujo de entrada.
+            BufferedReader bufferLectura = new BufferedReader(archivo);
+            // Crea Arraylist para recibir los datos importados
+            ArrayList<String> listaImportada = new ArrayList<>();
+            String linea;
+            // Mientras haya nueva linea para leer traer a listaimportada
+            while ((linea = bufferLectura.readLine()) != null) {
+                listaImportada.add(linea);
+            }
+            // Cierra buffer temporal de lectura
+            bufferLectura.close();
+            // Cierra archivo que se esta leyendo
+            archivo.close();
+            return listaImportada;
+        }
+        //
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
