70add92d5386ced5c20cd5404a7b0f4f83f2adc2
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2023-05-16 07:51:09 -0500

    methods updates

--- Recordatorio.java
+++ Recordatorio.java
@@ -10,6 +10,7 @@ import java.time.LocalTime; //Importamos la hora local de la aplicación donde s
  * @author camilo_p & andres_s
  */
 public class Recordatorio {
+    private int tiempo;
     private Integer id;
     private Integer intervalo;
     private Date ultimoRecordatorio;
@@ -44,4 +45,27 @@ public class Recordatorio {
         //TODO: implementar una notificación
         System.out.println("Recordatorio: " + message);
     }
+
+
+    // Metodo que calcula los minutos que hay entre que se despierta y se duerme una persona.
+    public int calcularHoras(int horaDespierto, int minutoDespierto, int horaDormir, int minutoDormir)
+    {
+        int intervaloHoras = ((12 + horaDormir) - horaDespierto);
+        int intervaloMinutos = minutoDespierto - minutoDormir;
+        if(intervaloMinutos < 0)
+        {
+            int minutos = (-1) * intervaloMinutos;
+            tiempo = (intervaloHoras*60) + minutos;
+        }
+        else if(intervaloMinutos > 0)
+        {
+            tiempo = (intervaloHoras*60) - intervaloMinutos;
+        }
+        else
+        {
+            tiempo = intervaloHoras*60;
+        }
+        return tiempo;
+    }
+
 }
